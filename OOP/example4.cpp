6736d0053aed784ec2d74e2b78d0826c655c1fe3
peace-hue <kpeace2023@gmail.com>
2025-02-20 09:29:26 -0800

    example4.cpp

---
+++ OOP/example4.cpp
@@ -0,0 +1,6 @@
+int a = 10, b = 3;
+cout << "Sum: " << a + b << endl;
+cout << "Difference: " << a - b << endl;
+cout << "Product: " << a * b << endl;
+cout << "Quotient: " << a / b << endl;
+cout << "Remainder: " << a % b << endl;
